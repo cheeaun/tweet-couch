a73d2eebd30329916b0def30f60a8496823edf0e
Lim Chee Aun <cheeaun@gmail.com>
2014-02-05 22:43:47 -0800

    Remove supertweet, use OAuth with twit module

--- twitter2couch.js
+++ twitter2couch.js
@@ -7,15 +7,22 @@ nconf.argv()
 	.file(path.join(__dirname, 'config.json'))
 	.defaults({
 		user_id: '',
-		supertweet_auth: 'user:pass',
-		couchdb_url: 'http://localhost:5984'
+		couchdb_url: 'http://localhost:5984',
+		consumer_key: '',
+		consumer_secret: '',
+		access_token: '',
+		access_token_secret: ''
 	});
 
 var user_id = nconf.get('user_id');
-var auth = nconf.get('supertweet_auth');
+var Twit = require('twit');
+var twit = new Twit({
+	consumer_key: nconf.get('consumer_key'),
+	consumer_secret: nconf.get('consumer_secret'),
+	access_token: nconf.get('access_token'),
+	access_token_secret: nconf.get('access_token_secret')
+});
 
-var http = require('http');
-var qs = require('querystring');
 var nano = require('nano')(nconf.get('couchdb_url'));
 var tweets = nano.use('tweets');
 
@@ -25,49 +32,31 @@ tweets.view('tweets', 'by_created_date', {
 }, function(e, body){
 	if (e) throw e;
 	if (body.rows && body.rows.length){
+		var url = 'statuses/user_timeline';
 		var since_id = body.rows[0].id;
 
-		var host = 'api.supertweet.net';
-		var params = {
+		console.log('Requesting ' + url);
+		twit.get(url, {
 			user_id: user_id,
 			since_id: since_id,
 			count: 200,
 			include_rts: true
-		};
-		var path = '/1.1/statuses/user_timeline.json?' + qs.stringify(params);
-		console.log('Requesting ' + host + path);
-		var request = http.get({
-			host: host,
-			path: path,
-			auth: auth
-		});
-
-		request.on('response', function(response){
-			if (response.statusCode != 200){
-				console.error('Response not 200 OK');
-				console.error(response);
+		}, function(e, data){
+			if (e){
+				throw e;
 				return;
 			}
 
-			var body = '';
-			response.on('data', function(chunk){ body += chunk });
-			response.on('end', function(){
-				var data = JSON.parse(body);
-				if (data && data.length >= 1){
-					data.forEach(function(d){
-						d._id = d.id_str; // doc _id
-					});
-					tweets.bulk({docs: data}, function(err, response){
-						if (err) throw err;
-						var docIds = response.map(function(r){
-							return r.id;
-						});
-						if (docIds.length) console.log(docIds.length + ' doc(s) added: ' + docIds.join(', '));
-					});
-				}
+			data.forEach(function(d){
+				d._id = d.id_str; // doc _id
 			});
-		}).on('error', function(e){
-			throw e;
+			tweets.bulk({docs: data}, function(err, response){
+				if (err) throw err;
+				var docIds = response.map(function(r){
+					return r.id;
+				});
+				if (docIds.length) console.log(docIds.length + ' doc(s) added: ' + docIds.join(', '));
+			});
 		});
 	}
 });
